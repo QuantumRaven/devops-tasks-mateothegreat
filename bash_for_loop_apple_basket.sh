02f3266efc7bd8f8f5001b31adad41d5be8c989f
QuantumRaven <quantum-raven@outlook.com>
2021-07-21 06:48:52 -0400

    For loop in bash

---
+++ bash_for_loop_apple_basket.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+FRUIT="apple"
+
+for FRUIT in 1 2 3 4 5
+do
+  echo $FRUIT apple
+done
---
+++ py_for_loop_apple_basket.py
@@ -0,0 +1,13 @@
+##############
+# Apple loop
+##############
+
+# Assign apple to fruit
+fruit = 'apple'
+
+# Number of apples
+num = 1
+
+# Begin for loop of apples
+for i in fruit:
+    print(num,fruit)
